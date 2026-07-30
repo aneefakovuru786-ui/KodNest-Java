ab6acaea06dc081a99d265ece463dc929b57c79c
Aneefa <aneefakovuru786@gmail.com>
2026-07-30 13:01:11 +0530

    add one mmore line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
        System.out.println("Welcome to the java track");
        System.out.println("I already know how to build programming logic.");
        System.out.println("now I am learning how to express that logic using java.");
+       System.out.println("Now I know how to use git and github");
     }
 }
